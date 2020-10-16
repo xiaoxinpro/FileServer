1490f9969cd613884bd81211e5a39adc2ba53bd1
chishin <pro@xxgzs.org>
2020-10-16 09:01:59 +0800

    增加启动接口

--- dockerfile
+++ dockerfile
@@ -3,18 +3,20 @@ MAINTAINER chishin <pro@xxgzs.org>
 
 WORKDIR /
 ENV VERSION 0.0.1
+ENV PORT 8080
+ENV PATH /web/www
 
 RUN apk update && \
     apk add ca-certificates && \
     update-ca-certificates && \
     apk --no-cache add openssl wget && \
-    mkdir -p /web/www && \
+    mkdir -p -m 777 /web/www && \
     cd /web && \
     wget https://github.com/xiaoxinpro/FileServer/raw/main/build/FileServer_linux_amd64 -O ./FileServer && \
     chmod 777 /web/FileServer
 
-VOLUME /web/www
+VOLUME $PATH
 
-EXPOSE 8080
+EXPOSE $PORT
 
-CMD /web/FileServer
+CMD /web/FileServer -c env
--- main.go
+++ main.go
@@ -1,10 +1,53 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 )
 
+var (
+	ConfigSource string
+	WebPort int
+	WebPath string
+)
+
+func init() {
+	flag.StringVar(&ConfigSource,"c", "default", "config source default or env.")
+	flag.IntVar(&WebPort,"port", 8080, "web port.")
+	flag.StringVar(&WebPath,"path", "./www", "web path.")
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./www")))
-	http.ListenAndServe(":8080", nil)
+	flag.Parse()
+
+	if ConfigSource == "env" {
+		WebPort = getEnvInt("PORT", WebPort)
+		WebPath = getEnvString("PATH", WebPath)
+	}
+
+	fmt.Printf("File Server Port:%d Path:%s", WebPort, WebPath)
+
+	http.Handle("/", http.FileServer(http.Dir(WebPath)))
+	http.ListenAndServe(fmt.Sprintf(":%d", WebPort), nil)
+}
+
+func getEnvString(name string, value string) string {
+	ret := os.Getenv(name)
+	if ret == "" {
+		return value
+	} else {
+		return ret
+	}
+}
+
+func getEnvInt(name string, value int) int {
+	env := os.Getenv(name)
+	if ret, err := strconv.Atoi(env); env == "" || err != nil {
+		return value
+	} else {
+		return ret
+	}
 }
